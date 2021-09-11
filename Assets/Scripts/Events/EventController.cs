d4f866dcaf433bda35488b89be49dd5d03adad8b
Your Name <jacob_darvin@dlsu.edu.ph>
2021-09-11 10:17:17 +0800

    ANIMATION MOVEMENTS

--- Assets/Scripts/Events/EventController.cs
+++ Assets/Scripts/Events/EventController.cs
@@ -5,17 +5,22 @@ using UnityEngine;
 public class EventController : MonoBehaviour
 {
     public int id;
+    private Animator anim;
     [SerializeField] AudioSource audioTake;
+    [SerializeField] GameObject toCleanItem;
 
     void Start()
     {
         EventBroadcaster.current.onInteract += ContinueStory;
+        anim = GetComponent<Animator>();
+        anim.enabled = false;  //disable animation states by default.  
     }
 
     public void ContinueStory(int id)
     {
         if (id == this.id)
         {
+            anim.enabled = true;
             audioTake.Play();
         }
     }
